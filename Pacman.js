7d3612b80975576c234c76ac45babf58e88708f2
Ugur Tigu <ugurtigu@gmail.com>
2020-10-10 14:18:30 +0200

    initial commit of the game NO MOBILE buttons

---
+++ Pacman.js
@@ -0,0 +1,12 @@
+import { OBJECT_TYPE, DIRECTIONS } from './setup';
+import Character from './Character';
+
+class Pacman extends Character {
+    constructor(speed, startPos) {
+        super(speed, startPos);
+    }
+
+
+}
+
+export default Pacman;
